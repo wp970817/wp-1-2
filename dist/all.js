d6d3c11074ec02792e760cc47e19935c7535a8f6
wp <wp19970817@qq.com>
2019-01-02 09:50:52 +0800

    测试完毕

---
+++ dist/all.js
@@ -0,0 +1,2 @@
+btn1.onclick=function(){alert("登陆成功了啦")};
+btn2.onclick=function(){alert("注册成功")};
--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,12 @@ var server = require("gulp-webserver");//服务
 var uglify = require("gulp-uglify");//压缩js
 var concat = require("gulp-concat");//合并js
 
+
+var fs = require("fs");
+var path= require("path");
+var url = require("url");
+
+//css
 gulp.task("sass",function(){
     return gulp.src("./src/styles/*.scss")
     .pipe(sass())
@@ -15,6 +21,7 @@ gulp.task("sass",function(){
     .pipe(gulp.dest("./src/css"));
 })
 
+//js
 gulp.task("conjs",function(){
     return gulp.src("./src/scripts/*.js")
     .pipe(uglify())
@@ -22,6 +29,8 @@ gulp.task("conjs",function(){
     .pipe(gulp.dest("./src/js"));
 })
 
+
+//监听
 gulp.task("watch",function(){
     gulp.watch("./src/styles/*.scss",gulp.series("sass"));
     gulp.watch("./src/scripts/*.js",gulp.series("conjs"));
@@ -33,5 +42,31 @@ gulp.task("server",function(){
     .pipe(server({
         port : 9990,
         open:true,
+        middleware : function(req,res,next){
+            if(req.url === "./favicon.ico"){
+                return res.end();
+            }
+            console.log(req.url)
+            // var pathname = url.parse(req.url).pathname;
+            // if(pathname === "/"){
+            //     res.end(fs.readFileSync(__dirname,"src","index.html"));
+            // }
+            if(req.url === "/"){
+                res.end(fs.readFileSync("./src/index.html"))
+            }else{
+                var pathname = url.parse(req.url).pathname;
+                console.log(pathname)
+                res.end(fs.readFileSync("./src/"+pathname))
+            }
+            
+        }
     }))
 })
+
+gulp.task("build",function(){
+    return gulp.src("./src/css/style.css")
+    .pipe(gulp.src("./src/js/all.js"))
+    .pipe(gulp.dest("./dist"))
+})
+
+gulp.task("default",gulp.series("server","watch"));
